1a700af8bf143de72e13804d8bc6ab807297c3b3
Niranjan😎 <niranjanbodiga3535@gmail.com>
2018-07-17 22:46:05 +0530

    Adding template file

---
+++ template.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define sd(x) scanf("%d",&x)
+#define pb(x) push_back(x)
+#define pf(x) printf("%d\n",x)
+#define INF 1000000000
+#define lim 1010
+
+int main() {
+
+	return 0;
+}
